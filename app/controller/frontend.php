cb02edb62f117b71c3358318db7f5e975320f30e
Tr@ppeur1975 <trappeur1975photmail.com>
2021-01-22 17:05:18 +0400

    use of AltoRouter () and refresh the code of index.php and frontend.php

--- app/controller/frontend.php
+++ app/controller/frontend.php
@@ -4,17 +4,25 @@ use App\Model\PostManager;
 // require_once('../vendor/autoload.php');
 
 // URL : http://localhost/ocs_projet5_creez-votre-premier-blog-en_php/?action=post&id=1
-function post()
+// function post()
+// {
+//     $postManager = new PostManager(); // Création de l'objet manger de post
+//     $post = $postManager->getPost($_GET["id"]);
+
+//     require('../app/view/frontend/postView.php');
+// }
+function post($id)
 {
     $postManager = new PostManager(); // Création de l'objet manger de post
-    $post = $postManager->getPost($_GET["id"]);
+    $post = $postManager->getPost($id);
 
     require('../app/view/frontend/postView.php');
 }
 
+
 // A MODIFIER POUR GERER L HYDRATATION
 // URL : http://localhost/ocs_projet5_creez-votre-premier-blog-en_php/?action=listPosts
-function ListPosts()
+function listPosts()
 {
     $postManager = new \App\Model\PostManager(); // Création d'un objet
     $listPosts = $postManager->getListPosts(); // Appel d'une fonction de cet objet
--- public/index.php
+++ public/index.php
@@ -1,19 +1,41 @@
 <?php //NOTRE ROUTEUR (indique au controller quel function lancer en fonction des paramétres (notamment ici "action") dans url de la page du site
+require('../vendor/altorouter/altorouter/AltoRouter.php');
 require('../App/controller/frontend.php');
 require_once('../vendor/autoload.php');
 
-try { // On essaie de faire des choses
-    if (isset($_GET['action'])) { // URL : http://localhost/ocs_projet5_creez-votre-premier-blog-en_php/?action=listPosts
-        if ($_GET['action'] == 'listPosts') {
-            listPosts();
-        } elseif ($_GET['action'] == 'post') { // URL : http://localhost/ocs_projet5_creez-votre-premier-blog-en_php/?action=post&id=1
-            if (isset($_GET['id']) && $_GET['id'] > 0) {
-                post();
-            } else {
-                // Erreur ! On arrête tout, on envoie une exception, donc au saute directement au catch
-                throw new Exception('Aucun identifiant de billet envoyé');
-            }
-        }
+$router = new AltoRouter();
+
+$router->map('GET', '/', function () {  // pour la route http://localhost:8000/
+    echo 'salut nicolas';
+});
+
+$router->map('GET', '/listposts', function (){  // pour la route http://localhost:8000/listposts
+    listPosts();
+});
+
+$router->map('GET', '/post/[i:id]', function ($id){  // pour la route http://localhost:8000/post/1 ou http://localhost:8000/post/2 ou ....
+    post($id);
+});
+
+$match = $router->match();
+if($match !== null){
+    call_user_func_array($match['target'], $match['params']); //pour gerer les arguments appeller dans la closure
+    // $match['target']($match['params']['id']);
+    // $match['target']();
+}
+
+// try { 
+//     if (isset($_GET['action'])) { // URL : http://localhost/ocs_projet5_creez-votre-premier-blog-en_php/?action=listPosts
+//         if ($_GET['action'] == 'listPosts') {
+//             listPosts();
+//         } elseif ($_GET['action'] == 'post') { // URL : http://localhost/ocs_projet5_creez-votre-premier-blog-en_php/?action=post&id=1
+//             if (isset($_GET['id']) && $_GET['id'] > 0) {
+//                 post();
+//             } else {
+//                 // Erreur ! On arrête tout, on envoie une exception, donc au saute directement au catch
+//                 throw new Exception('Aucun identifiant de billet envoyé');
+//             }
+//         }
 
         //  // VOIR CI CE ELSE IF EST CORRECT POUR AJOUTER DES COMMENTAIRE A UN POST
         //  elseif ($_GET['action'] == 'addComment') {
@@ -32,10 +54,10 @@ try { // On essaie de faire des choses
         //      }
         //  }
 
-    } else {
-        //  listPosts();
-        echo "salut internaute";
-    }
-} catch (Exception $e) { // S'il y a eu une erreur, alors...
-    echo 'Erreur : ' . $e->getMessage();
-}
+//     } else {
+//         //  listPosts();
+//         echo "salut internaute";
+//     }
+// } catch (Exception $e) { // S'il y a eu une erreur, alors...
+//     echo 'Erreur : ' . $e->getMessage();
+// }
